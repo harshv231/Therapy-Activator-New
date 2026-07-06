2ded18288ab0ad789f7459e6265e26606272875c
Harshkumar Vankhede <harshvankhede231@gmail.com>
2026-07-06 20:10:04 +0530

    Fix import path for Sidebar component

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -4,7 +4,7 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { AdminContext } from './context/AdminContext';
 import Navbar from './components/Navbar';
-import Sidebar from './components/sidebar';
+import Sidebar from './components/Sidebar';
 import { Route, Routes } from 'react-router-dom';
 import Dashboard from './pages/Admin/Dashboard';
 import AllApointment from './pages/Admin/AllApointment';
